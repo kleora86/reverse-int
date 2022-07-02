2a637894372c19c925fbc3bbe48b1ed88c17495b
kleora86 <Random_les@mail.ru>
2022-07-03 00:35:16 +0300

    hhz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    n = n + "";
+    if (n>0) {
+  return parseInt(n.split("").reverse().join(""));}
+  else {
+    return parseInt(n.split("").reverse().join(""));}
 }
